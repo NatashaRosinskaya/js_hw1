15763feab6883f8a8c8eba6f72b569a771f278f3
NataRosinskaya <NataRosinskaya@yandex.by>
2021-05-30 23:36:33 +0300

    Commit

---
+++ script.js
@@ -0,0 +1,97 @@
+let a1 = 5 % 3;
+console.log(a1, typeof a1);
+
+let a2 = 3 % 5;
+console.log(a2, typeof a2);
+
+let a3 = 5 + '3';
+console.log(a3, typeof a3);
+
+let a4 = '5' - 3;
+console.log(a4, typeof a4);
+
+let a5 = 75 + 'кг';
+console.log(a5, typeof a5);
+
+let a6 = 785 * 653;
+console.log(a6, typeof a6);
+
+let a7 = 100 / 25;
+console.log(a7, typeof a7);
+
+let a8 = 0 * 0;
+console.log(a8, typeof a8);
+
+let a9 = 89 / 0;
+console.log(a9, typeof a9);
+
+let a10 = 0 / 2;
+console.log(a10, typeof a10);
+
+let a11 = 92 + 2;
+console.log(a11, typeof a11);
+
+let a12 = 5 - 98;
+console.log(a12, typeof a12);
+
+let a13 = (8 + 56 * 4) / 5;
+console.log(a13, typeof a13);
+
+let a14 = (9 - 12) * 7 / (5 + 2);
+console.log(a14, typeof a14);
+
+let a15 = + "123"
+console.log(a15, typeof a15);
+
+let a16 = 1 || 0;
+console.log(a16, typeof a16);
+
+let a17 = false || true;
+console.log(a17, typeof a17);
+
+let a18 = true > 0;
+console.log(a18, typeof a18);
+
+// ----------
+
+let width = 23;
+let height = 10;
+let SPryam = width * height;
+console.log(SPryam + ' см');
+
+// ----------
+
+let h = 10;
+let pi = 3.14;
+let VCilindra = (pi * a7**a7) / (4 * h);
+console.log(VCilindra + ' м');
+
+// ----------
+
+let r = 5;
+let SKruga = pi * r**r;
+console.log(SKruga + ' cм');
+
+// ----------
+
+let a = 5;
+let b = 7;
+let STrap = ((a + b) / 2) * h;
+console.log(STrap + ' cм');
+
+// ----------
+
+let S = 2000000;
+let p = 0.1;
+let years = 5;
+
+let Pereplata = S * p * years;
+console.log(Pereplata);
+
+// ----------
+let c = 8;
+let d = 3;
+let x1 = ((16 - c) + 2 * d) / 2
+console.log (x1)
+
+
